4898b2f577d5ceb2f8ee678835a5a85b379d35f4
PedroB <bereilhp@gmail.com>
2023-05-20 18:29:54 +0200

    test 0 ok

---
+++ ejercicios/simulacro/fecha.js
@@ -0,0 +1,7 @@
+class Fecha {
+
+};
+
+module.exports = {
+    Fecha
+};
--- ejercicios/simulacro/fecha.test.js
+++ ejercicios/simulacro/fecha.test.js
@@ -1,3 +1,4 @@
+const {Fecha} = require("./fecha")
 describe("Check if they exists", () => {
     it("Class Fecha Exists", () => {
         expect(typeof(Fecha)).toBe("function");
