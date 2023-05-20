fd1d432b6efca9409641ee34ce27a8fdc9b251a5
PedroB <bereilhp@gmail.com>
2023-05-20 18:43:07 +0200

    test 3

--- ejercicios/simulacro/fecha.test.js
+++ ejercicios/simulacro/fecha.test.js
@@ -1,4 +1,4 @@
-const {Fecha} = require("./fecha")
+const {Fecha} = require("./fecha.js")
 describe("Check if they exists", () => {
     it("Class Fecha Exists", () => {
         expect(typeof(Fecha)).toBe("function");
@@ -14,5 +14,11 @@ describe("Function darFecha()",  () => {
     it("darFecha() exists", () => {
         const fecha = new Fecha();
         expect(typeof(fecha.darFecha)).toBe("function")
+    })
+
+    it("darFecha() simple test", () => {
+        const fechaSimple = new Fecha();
+        let fecha = new Date().toDateString();
+        expect(fechaSimple.darFecha()).toBe(fecha)
     })
 })
