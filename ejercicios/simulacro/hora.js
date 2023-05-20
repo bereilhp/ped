2acf458ac8d4bfedca70546218e474ff5b9f243e
PedroB <bereilhp@gmail.com>
2023-05-20 18:55:24 +0200

    test 7 ok

--- ejercicios/simulacro/hora.js
+++ ejercicios/simulacro/hora.js
@@ -1,8 +1,10 @@
 class Hora {
     darHora(){
-        
+        let hours = new Date().getHours();
+        let minutes = new Date().getMinutes();
+        let seconds = new Date().getSeconds();
+        return `${hours}:${minutes}:${seconds}`
     }
-
 };
 
 module.exports = {
